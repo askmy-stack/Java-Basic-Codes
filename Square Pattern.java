500b9d04aa3e3ec0e86dfb17de0c010cdbd9bba8
Abhinaysai Kamineni <66816045+askmy-stack@users.noreply.github.com>
2022-06-07 15:39:33 +0530

    Create Square Pattern.java

---
+++ Square Pattern.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.Scanner;
+
+public class Sqaure Pattern{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number:");
+        int n = sc.nextInt();
+        int i = 1;
+        while (i <= n) {
+            int j = 1;
+            while (j <= n) {
+                System.out.print("*");
+                j++;
+            }
+            i++;
+        }
+    }
+}
+
