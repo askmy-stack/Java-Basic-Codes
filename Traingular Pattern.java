11e4c9f9d40264b1e185a1bc684191c73f3fd3cc
Abhinaysai Kamineni <66816045+askmy-stack@users.noreply.github.com>
2022-06-07 15:20:17 +0530

    Update Traingular Pattern.java

--- Traingular Pattern.java
+++ Traingular Pattern.java
@@ -1,7 +1,7 @@
 package com.company;
 import java.util.Scanner;
 
-public class Practice {
+public class Traingular Pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please Enter the Number of Choice:");
